61bbad424c76115efba040b190f8b50dc8c10814
Oscar Cáceres <oscar_caceres2321@mesoamericana.edu.gt>
2021-03-19 10:56:01 -0600

    Creación de la clase estudiantes y de la clase libros con cada uno de sus atributos. Diseño de la interfaz gráfica para agregar estudiantes y libros.

---
+++ Parcial1/Libros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial1
+{
+    class Libros
+    {
+        string codigo;
+        string titulo;
+        string autor;
+        string año;
+
+        public string Codigo { get => codigo; set => codigo = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Autor { get => autor; set => autor = value; }
+        public string Año { get => año; set => año = value; }
+    }
+}
